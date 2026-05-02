e0ceb5c2e8372a6f6c3b0db125064f630eb99ee0
fahimad24 <mdtaraquerahmanfahim@gmail.com>
2026-05-03 01:06:27 +0600

    server API change

--- src/lib/getData.js
+++ src/lib/getData.js
@@ -1,17 +1,17 @@
 export const getAllCourses = async () => {
-    const res = await fetch('http://localhost:4000/courses');
+    const res = await fetch('https://skillsphere-server-oyxw.onrender.com/courses');
     const data = await res.json();
     return data;
 };
 
 export const getPopularCourses = async () => {
-    const res = await fetch('http://localhost:4000/courses?popular=true');
+    const res = await fetch('https://skillsphere-server-oyxw.onrender.com/courses?popular=true');
     const data = await res.json();
     return data;
 };
 
 export const getCourseById = async (id) => {
-    const res = await fetch(`http://localhost:4000/courses/${id}`);
+    const res = await fetch(`https://skillsphere-server-oyxw.onrender.com/courses/${id}`);
     const data = await res.json();
     return data;
 };
