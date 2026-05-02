b5848859556133d8fccf9776e1a60b56ba7d28f4
fahimad24 <mdtaraquerahmanfahim@gmail.com>
2026-05-03 01:54:06 +0600

    cookies change

--- src/proxy.js
+++ src/proxy.js
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 
 // This function can be marked `async` if using `await` inside
 export function proxy(request) {
-    const token = request.cookies.get('better-auth.session_token')?.value;
+    const token = request.cookies.get('__Secure-better-auth.session_token')?.value;
     const { pathname } = request.nextUrl;
     console.log('Token in proxy:', token);
 
